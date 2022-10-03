6f75ff00c8f0c9d47f90888f7b4c855903dd34ff
harshmangalam <harshdev8218@gmail.com>
2022-10-03 13:48:11 +0530

    updated product info

--- app/components/product/ProductCard.tsx
+++ app/components/product/ProductCard.tsx
@@ -2,29 +2,23 @@ import { Card, Image, Stack, Text, Title } from "@mantine/core";
 import { Link } from "@remix-run/react";
 
 interface ProductCardProps {
-  id: string;
   title: string;
   thumbnail: string;
-  price: string;
   handle: string;
 }
-export default function ProductCard() {
+export default function ProductCard({
+  handle,
+  thumbnail,
+  title,
+}: ProductCardProps) {
   return (
-    <Card p="xl" component={Link} to="/products">
+    <Card p="xl" component={Link} to={`/products/${handle}`}>
       <Card.Section>
-        <Image
-          src={
-            "http://localhost:8000/_next/image?url=https%3A%2F%2Fmedusa-public-images.s3.eu-west-1.amazonaws.com%2Fblack_hoodie_front.png&w=1920&q=75"
-          }
-          alt={"Alt"}
-          height={340}
-          width="100%"
-        />
+        <Image src={thumbnail} alt={"Alt"} height={340} width="100%" />
       </Card.Section>
 
       <Stack mt="md" spacing={"xs"}>
-        <Text size={"xl"}>Medusa Longsleeve</Text>
-        <Title size={"lg"}>$19.50</Title>
+        <Title size={"h4"}>{title}</Title>
       </Stack>
     </Card>
   );
--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,23 +1,31 @@
 import { Container, SimpleGrid } from "@mantine/core";
 import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import ProductCard from "~/components/product/ProductCard";
 import { medusaClient } from "~/lib/medusa";
 
 export const loader: LoaderFunction = async () => {
   try {
     const medusa = medusaClient();
-    const products = await medusa.products.list();
-    return json(products);
+    const res = await medusa.products.list();
+    return json(res.products);
   } catch (error) {
     throw error;
   }
 };
 export default function ProductsRoute() {
+  const loaderData = useLoaderData();
+  console.log(loaderData);
   return (
     <Container size={"xl"}>
       <SimpleGrid cols={3}>
-        {[...new Array(12)].map((product) => (
-          <ProductCard {...product} />
+        {loaderData?.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            handle={product.handle}
+            thumbnail={product.thumbnail}
+          />
         ))}
       </SimpleGrid>
     </Container>

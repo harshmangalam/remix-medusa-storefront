f34f39b18340d005f61a9e6b9f2991dd4ff0e432
harshmangalam <harshdev8218@gmail.com>
2022-10-03 13:35:36 +0530

    updated product

--- app/components/product/ProductCard.tsx
+++ app/components/product/ProductCard.tsx
@@ -1,4 +1,5 @@
-import { Card, Image, Text, Title } from "@mantine/core";
+import { Card, Image, Stack, Text, Title } from "@mantine/core";
+import { Link } from "@remix-run/react";
 
 interface ProductCardProps {
   id: string;
@@ -9,20 +10,22 @@ interface ProductCardProps {
 }
 export default function ProductCard() {
   return (
-    <Card>
+    <Card p="xl" component={Link} to="/products">
       <Card.Section>
         <Image
           src={
             "http://localhost:8000/_next/image?url=https%3A%2F%2Fmedusa-public-images.s3.eu-west-1.amazonaws.com%2Fblack_hoodie_front.png&w=1920&q=75"
           }
           alt={"Alt"}
-          height={160}
+          height={340}
+          width="100%"
         />
       </Card.Section>
-      <Card.Section>
-        <Title>Medusa Longsleeve</Title>
-        <Text>$19.50</Text>
-      </Card.Section>
+
+      <Stack mt="md" spacing={"xs"}>
+        <Text size={"xl"}>Medusa Longsleeve</Text>
+        <Title size={"lg"}>$19.50</Title>
+      </Stack>
     </Card>
   );
 }

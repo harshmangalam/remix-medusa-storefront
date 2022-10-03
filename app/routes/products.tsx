c5a2c10703e07279140e191c2f08b211b8b148e6
harshmangalam <harshdev8218@gmail.com>
2022-10-03 13:37:46 +0530

    added loader

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,6 +1,17 @@
 import { Container, SimpleGrid } from "@mantine/core";
+import { json, LoaderFunction } from "@remix-run/node";
 import ProductCard from "~/components/product/ProductCard";
+import { medusaClient } from "~/lib/medusa";
 
+export const loader: LoaderFunction = async () => {
+  try {
+    const medusa = medusaClient();
+    const products = await medusa.products.list();
+    return json(products);
+  } catch (error) {
+    throw error;
+  }
+};
 export default function ProductsRoute() {
   return (
     <Container size={"xl"}>
